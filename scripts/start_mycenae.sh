8e2d05d13cc93ad476c1b85ce3223b16d0997b52
rnojiri <nojiri.rafael@gmail.com>
2017-11-29 12:07:46 -0200

    Adding the new branch scylla from rnojiri fork. (#12)
    * Adapting mycenae to use scylla db.
    
    * Removing consul.
    
    * New docker scripts + gocql update.
    
    * Changing the docker image to use fedora 25.
    
    * Adjusting scripts to run mycenae too.
    
    * Updating keyspace creation for scylla.

---
+++ scripts/start_mycenae.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+POD_NAME="mycenae${1}"
+docker rm -f "${POD_NAME}"
+
+if ! make -C "${GOPATH}/src/github.com/uol/mycenae/" build ; then
+    exit 1
+fi
+
+scyllaIPs=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" scylla1 scylla2 scylla3 | sed 's/^.*$/"&"/' | tr '\n' ',' | sed 's/.$//')
+elasticIP=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" elasticsearch)
+
+sed -i 's/nodes = \[[^]]*\]/nodes = \['$scyllaIPs'\]/' ../config-scylla.toml
+sed -i 's/"[^:]*:9200"/"'$elasticIP':9200"/' ../config-scylla.toml
+
+pod_arguments=(
+	'-it'
+    '--detach'
+    '--name' "${POD_NAME}"
+    '--volume' "${GOPATH}/src/github.com/uol/mycenae/mycenae:/tmp/mycenae"
+    '--volume' "${GOPATH}/src/github.com/uol/mycenae/config-scylla.toml:/config.toml"
+    '--entrypoint' '/tmp/mycenae'
+    '-p' '8787:8787'
+)
+
+dockerCmd="docker run ${pod_arguments[@]} ubuntu:xenial"
+eval "$dockerCmd"
+echo "$dockerCmd"
+
+echo 'Mycenae OK'
+
+docker logs "${POD_NAME}"
---
+++ scripts/start_scylla.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+pod_name="scylla$1"
+
+pod_arguments=(
+    '-d'
+    '-it'
+    '--name' "${pod_name}"
+    '-e' 'CONFIG=/opt/scylla/conf/scylla.yaml'
+)
+
+if [ $1 -gt 1 ]
+    then
+        seedIP="$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' scylla1)"
+        pod_arguments[${#pod_arguments[@]}]="-e SEEDS=$seedIP"
+else
+    pod_arguments[${#pod_arguments[@]}]="-e SEEDS=127.0.0.1"
+fi
+
+cmd="docker run ${pod_arguments[@]} jenkins.macs.intranet:5000/mycenae/scylla-fedora:v1"
+eval "${cmd}"
+echo "${cmd}"
+
+echo "${pod_name} OK"
